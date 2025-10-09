787f05bfb40ad7ad07bfea36dc5b904a62f85d50
austinxduong <austinxduong@gmail.com>
2025-10-08 18:26:38 -0700

    add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
 } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
 
 const App = () => {
   return (
@@ -13,8 +15,30 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+
+         {/* protected routes */}
+        <Route path="/" element={<ProtectedRoute /> }>
+          <Route path="dashboard" element={<Dasboard />} />
+          <Route path="invoices" element={<AllInvoices />} />
+          <Route path="invoices/new" element={<CreateInvoice />} />
+          <Route path="invoices/:id" element={<InvoiceDetail />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </div>
   )
 }
