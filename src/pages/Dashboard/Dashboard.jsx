b4780777b822b25002f020238207b6fc592fff4b
austinxduong <austinxduong@gmail.com>
2025-10-26 19:36:52 -0700

    add dashboard spinner

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,66 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { Loader2, FileText, DollarSign, Plus } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
+import Button from "../../components/ui/Button"
 
 const Dashboard = () => {
+
+  const [stats, setStats] = useState({
+    totalInvoices: 0,
+    totalPaid: 0,
+    totalUnpaid: 0,
+  });
+
+  const [recentInvoices, setRecentInvoices] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+
+    };
+
+    fetchDashboardData();
+  }, []);
+
+  const statsData = [
+    {
+      icon: FileText,
+      label: "Total Invoices",
+      value: stats.totalInvoices,
+      color: "blue",
+    },
+    {
+      icon: DollarSign,
+      label: "Total Paid",
+      value: `${stats.totalPaid.toFixed(2)}`,
+      color: "emerald",
+    },
+    {
+      icon: DollarSign,
+      label: "Total Unpaid",
+      value: `${stats.totalUnpaid.toFixed(2)}`,
+      color: "red",
+    },
+  ];
+
+  const colorClasses = {
+    blue: { bg: "bg-blue-100", text: "text-blue-600" },
+    emerald: { bg: "bg-emerald-100", text: "text-emerald-600" },
+    red: { bg:"bg-red-100", text: "text-red-600" },
+  };
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <Loader2 className="w-8 h-8 animate-spin text-blue-600" />
+      </div>
+    );
+  }
+
   return (
     <div>Dashboard</div>
   )
