31ff90717506af7fab2d6b4f35d0848d40221cc4
austinxduong <austinxduong@gmail.com>
2025-10-28 19:58:42 -0700

    ai insights description

--- src/components/AIInsightsCard.jsx
+++ src/components/AIInsightsCard.jsx
@@ -1,39 +1,36 @@
-import { useState, useEffect } from 'react';
-import { Lightbulb } from 'lucide-react'
-import axiosInstance from '../utils/axiosInstance'
-import { API_PATHS } from '../utils/apiPaths';
+import { useState, useEffect } from "react";
+import { Lightbulb } from "lucide-react";
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/apiPaths";
 
 const AIInsightsCard = () => {
+  const [insights, setInsights] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [insights, setInsights] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
-
-
-useEffect(() => {
+  useEffect(() => {
     const fetchInsights = async () => {
-        try {
-            const response = await axiosInstance.get(API_PATHS.AI.GET_DASHBOARD_SUMMARY);
-            setInsights(response.data.insights || []);
-        } catch (error) {
-            console.error("Failed to fetch AI insights", error)
-            setInsights([]); // empty array after error
-        } finally {
-            // setIsLoading(false)
-        }
-    }
-    fetchInsights();
-}, [])
+      try {
+        const response = await axiosInstance.get( API_PATHS.AI.GET_DASHBOARD_SUMMARY);
+        setInsights(response.data.insights || []);
+      } catch (error) {
+        console.error("Failed to fetch AI insights", error);
+        setInsights([]); // Set empty array on error
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
+    fetchInsights();
+  }, []);
 
   return <div className="bg-white p-4 rounded-xl border border-slate-200 shadow-sm shadow-gray-100">
       <div className="flex items-center mb-4">
         <Lightbulb className="w-6 h-6 text-yellow-500 mr-3" />
-        <h3 className="text-lg font-semibold text-slate-900 ">AI Insights</h3>
+        <h3 className="text-lg font-semibold text-slate-900">AI Insights</h3>
       </div>
-      {isLoading ? (
-        // if ai insight is loading/fetching, shows flashing cards
+      {isLoading ? ( // if ai insight is still fetching, show flashing cards via. ternary operator
         <div className="space-y-3 animate-pulse">
-          <div className="h-4 bg-slate-200 rounded w-3/4" ></div>
+          <div className="h-4 bg-slate-200 rounded w-3/4"></div>
           <div className="h-4 bg-slate-200 rounded w-5/6"></div>
           <div className="h-4 bg-slate-200 rounded w-1/2"></div>
         </div>
@@ -46,5 +43,6 @@ useEffect(() => {
       )}
     </div>
 };
+
 
 export default AIInsightsCard
