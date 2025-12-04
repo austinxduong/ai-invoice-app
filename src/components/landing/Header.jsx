673ac4f9a5813fbb9b6a5a1b68ba675847f954f0
austinxduong <austinxduong@gmail.com>
2025-12-04 02:35:26 -0800

    bug: fix typo

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -120,7 +120,7 @@ const Header = () => {
                                 Testimonials
                             </a>
                             <a
-                                href="team"
+                                href="/team"
                                 className="block px-4 py-3 text-gray-600 hover:text-gray-900 hover:bg-gray-50 font-medium transition colors duration-200"
                             >
                                 Our team
