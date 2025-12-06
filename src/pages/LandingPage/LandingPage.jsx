02cb004d480fa87860d4be3497ec34289435d114
austinxduong <austinxduong@gmail.com>
2025-12-05 18:40:07 -0800

    fix file structure

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import Hero from '../../components/landing/Hero'
 import Features from '../../components/landing/Features'
 import { Testimonials }  from '../../components/landing/Testimonials'
 import Footer from '../../components/landing/Footer'
-import OurTeam from '../Team/OurTeam'
+import OurTeam from '../../components/landing/Team'
 
 
 
